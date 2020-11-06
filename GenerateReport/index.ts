110bdec0789be273804506fff06324986b7d9dc6
Matthew Evans <matthew@evans99.co.uk>
2020-11-06 15:07:37 +0000

    feat(generate report): allow date range to be entered

--- GenerateReport/index.ts
+++ GenerateReport/index.ts
@@ -6,8 +6,15 @@ import { flatMap, uniq } from 'lodash';
 import { queryList, uniqueByPartitionAndRowKey } from '../common/functions';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    const startDate = req.query['start'];
+    const endDate = req.query['end'];
+    let dateQuery = '';
+    // Search for reports between the two queried dates
+    if (startDate && endDate) {
+        dateQuery = ` and (ReportDate ge datetime'${new Date(startDate).toISOString()}' and ReportDate le datetime'${new Date(endDate).toISOString()}')`;
+    }
     // Get all reported users
-    const reportedUsers = await queryList<UserEntity>(usersTableClient, 'Reported eq true');
+    const reportedUsers = await queryList<UserEntity>(usersTableClient, `(Reported eq true)${dateQuery}`);
     // Get all checkins which reported users visited
     const primaryAffectedCheckins = uniqueByPartitionAndRowKey<CheckinEntity>(flatMap<CheckinEntity>(await Promise.all(
         reportedUsers.map(u => queryList(checkinsTableClient, `User eq '${ u.rowKey }'`)),
