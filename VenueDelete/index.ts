fdb7008dbbf6ba4f0bd5efdb52c9ca38338277f6
Matthew Evans <matthew@evans99.co.uk>
2020-11-06 14:21:32 +0000

    feat(venue delete): cascade to delete checkins as well

--- VenueDelete/index.ts
+++ VenueDelete/index.ts
@@ -1,11 +1,15 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions'
-import { defaultPartition, venuesTableClient } from '../common';
+import { checkinsTableClient, defaultPartition, venuesTableClient } from '../common';
+import { queryList } from '../common/functions';
+import { CheckinEntity } from '../common/models/checkin.model';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     const id = req.query.id;
     try {
         if (!id) throw { statusCode: 404, message: 'Not found' };
         await venuesTableClient.deleteEntity(defaultPartition, id);
+        const venueCheckins = await queryList<CheckinEntity>(checkinsTableClient, `PartitionKey eq '${id}'`);
+        await Promise.all(venueCheckins.map(checkin => checkinsTableClient.deleteEntity(checkin.partitionKey, checkin.rowKey)));
         context.res = {
             status: 204,
         };
